e458ce3f205a0cf9495976160e3fd7566c499a8a
Olivier Audet-Yang <oaudetyang@gmail.com>
2021-11-18 17:39:42 -0500

    Postgres

--- Shrimpbot/settings.py
+++ Shrimpbot/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "6s*jq30_3f-ie=&qmv3c4lfcs+cix7!7#h%tm((ncx)b2=mrfy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+if DEBUG:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "shrimp-chatbot.herokuapp.com"]
+else:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
+
 
 APPEND_SLASH = False
 
@@ -87,6 +89,8 @@ DATABASES = {
     }
 }
 
+if not DEBUG:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
