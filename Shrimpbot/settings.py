d8d861494b89019ea7b73b23a5ed28f69aa120cc
Olivier Audet-Yang <oaudetyang@gmail.com>
2021-11-18 19:59:46 -0500

    Secret key

--- Shrimpbot/settings.py
+++ Shrimpbot/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "6s*jq30_3f-ie=&qmv3c4lfcs+cix7!7#h%tm((ncx)b2=mrfy"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
