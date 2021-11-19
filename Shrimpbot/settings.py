21cf43a1b4eaf4dcd756b4cd9338f8e87646017b
Olivier Audet-Yang <oaudetyang@gmail.com>
2021-11-18 21:15:46 -0500

    stuf

--- Shrimpbot/settings.py
+++ Shrimpbot/settings.py
@@ -79,14 +79,8 @@ WSGI_APPLICATION = 'Shrimpbot.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
-
-if not DEBUG and os.environ.get('DATABASE_URL', None):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
